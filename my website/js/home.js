e42b2b032f7beb7826aeae0d868ae986007467db
oandasan90 <johnmarcoandasan29@gmail.com>
2025-04-30 10:09:29 +0800

    home.js

--- my website/js/home.js
+++ my website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'e4f0e9a44a7d1af902486d8eeaf783b5';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
